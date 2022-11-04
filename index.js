59bbc8cb42a1efc466bd8aab7d2de7ccdf85d0b2
nawalkanabee <nawalkanabee@gmail.com>
2022-11-04 20:21:02 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your classes here
+class Tree{
+    constructor(species){
+        this.species=species;
+    }
+   
+}
+ class Deciduous extends Tree{
+    constructor(species,name){
+        super(species);
+        this.name=name;
+    }
+    static definition(){
+        return(
+            super.definition() + " Deciduous trees shed their leaves annually."
+        );
+    }
+ }
+
+ class Evergreen extends Tree{
+    constructor(species,name){
+        super(species);
+        this.name=name;
+    }
+    static definition(){
+        return(
+            super.definition() + " Evergreens keep their leaves all year round."
+        );
+    }
+ }
